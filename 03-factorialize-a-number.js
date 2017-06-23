e4a605331e190e4d2a83be33d2cb61fbdd7f68d4
Alberto De Lucca <albertodelucca@gmail.com>
2017-06-22 21:41:41 -0400

    Solve exercise 03

---
+++ 03-factorialize-a-number.js
@@ -0,0 +1,20 @@
+// https://www.freecodecamp.com/challenges/factorialize-a-number
+
+function factorialize(num) {
+  if (num === 0){
+    return 1;
+  }
+
+  var array = [];
+
+  for (var c = 1; c <= num; c++){
+    array.push(c);
+  }
+  return array.reduce(
+    function(acum, val){
+      return acum * val;
+    }
+  );
+}
+
+factorialize(5);
