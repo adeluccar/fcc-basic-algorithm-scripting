e8aedaadddd88899cb6274370e8008b29ce9041f
Alberto De Lucca <albertodelucca@gmail.com>
2017-06-22 21:41:30 -0400

    Solve exercise 02

---
+++ 02-reverse-a-string.js
@@ -0,0 +1,8 @@
+// https://www.freecodecamp.com/challenges/reverse-a-string
+
+function reverseString(str){
+  str = str.split("").reverse().join("");
+  return str;
+}
+
+console.log(reverseString("Hello"));
