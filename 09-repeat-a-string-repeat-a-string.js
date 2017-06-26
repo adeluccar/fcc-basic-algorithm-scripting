e5224bca0c70f78e36e1d35d3d09a304ccdd9b0d
Alberto De Lucca <albertodelucca@gmail.com>
2017-06-25 21:23:19 -0400

    Solve exercise 09

---
+++ 09-repeat-a-string-repeat-a-string.js
@@ -0,0 +1,10 @@
+// https://www.freecodecamp.com/challenges/repeat-a-string-repeat-a-string
+
+function repeatStringNumTimes(str, num) {
+  if (num < 0) {
+    return "";
+  }
+  return str.repeat(num);
+}
+
+repeatStringNumTimes("abc", -3);
