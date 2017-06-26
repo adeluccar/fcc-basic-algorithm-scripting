af591b0acdb2f27240677e58bf880577930c724b
Alberto De Lucca <albertodelucca@gmail.com>
2017-06-25 22:05:59 -0400

    Solve exercise 10

---
+++ 10-truncate-a-string.js
@@ -0,0 +1,25 @@
+// https://www.freecodecamp.com/challenges/truncate-a-string
+
+function truncateString(str, num) {
+  var lessThanThree;
+
+  if (str.length <= num) {
+    return str;
+  }
+
+  if (num <= 3) {
+    lessThanThree = 0;
+  } else {
+    lessThanThree = 3;
+  }
+
+  var truncatedString = str.slice(0, num - lessThanThree);
+
+  if (str.length > num) {
+    return truncatedString + "...";
+  } else {
+    return truncatedString;
+  }
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length);
