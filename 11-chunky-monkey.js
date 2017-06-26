62a96e91e3e8b26a298e068bd004f8ec60f72f8c
Alberto De Lucca <albertodelucca@gmail.com>
2017-06-25 22:33:53 -0400

    Solve exercise 11

---
+++ 11-chunky-monkey.js
@@ -0,0 +1,18 @@
+// https://www.freecodecamp.com/challenges/chunky-monkey
+
+function chunkArrayInGroups(arr, size) {
+  var finalArray = [];
+
+  while(arr.length > 0) {
+    var sliceArray = arr.slice(0, size);
+    finalArray.push(sliceArray);
+
+    for (var i = 0; i < size; i++) {
+      arr.shift();
+    }
+  }
+
+  return finalArray;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 3);
