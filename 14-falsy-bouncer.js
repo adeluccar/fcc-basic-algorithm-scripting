3f4ef5014302f699aaa9176de5551ceff305a24e
Alberto De Lucca <albertodelucca@gmail.com>
2017-06-26 15:56:04 -0400

    Refactor exercise 14
    This seems more descriptive than using the Boolean constructor.

--- 14-falsy-bouncer.js
+++ 14-falsy-bouncer.js
@@ -1,7 +1,10 @@
 // https://www.freecodecamp.com/challenges/falsy-bouncer
 
 function bouncer(arr) {
- return arr.filter(Boolean);
+  return arr.filter( function( element ){
+    return element;
+  });
 }
+
 
 bouncer([7, "ate", "", false, 9, null]);
