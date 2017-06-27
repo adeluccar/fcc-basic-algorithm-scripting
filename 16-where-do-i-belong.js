86f20fd09889ad66850dc5347e749a5dbb62efe3
Alberto De Lucca <albertodelucca@gmail.com>
2017-06-27 02:14:54 -0400

    Solve exercise 16

---
+++ 16-where-do-i-belong.js
@@ -0,0 +1,21 @@
+// https://www.freecodecamp.com/challenges/where-do-i-belong
+
+/* we need this compareNumbers function to
+avoid Array.sort's Unicode sort */
+
+function compareNumbers(a, b) {
+  return a - b;
+}
+
+function getIndexToIns(arr, num) {
+  arr = arr.sort(compareNumbers);
+
+  var i = 0;
+  while (num > arr[i]) {
+    i++;
+  }
+
+  return i;
+}
+
+getIndexToIns([60, 40], 50);
